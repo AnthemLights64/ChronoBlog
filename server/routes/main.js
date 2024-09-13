4dd99b557f01555e1481134188ad870df31a3621
Wenlong Lu <yummybroccoli@outlook.com>
2024-09-14 01:13:39 +0800

    页面框架搭建

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,20 @@ const router = express.Router()
 
 // Routes
 router.get('', (req, res) => {
-    res.send("Hello ChronoBlog!")
+    const locals = {
+        title: 'Chrono Blog',
+        description: 'Simple Blog created with NodeJS, Express & MongoDb.'
+    }
+
+    res.render('index', { locals })
+})
+
+router.get('/about', (req, res) => {
+    res.render('about')
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact')
 })
 
 module.exports = router
