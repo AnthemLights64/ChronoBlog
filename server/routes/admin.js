dcdc65d56b8814d004c31ef82f8dcd93bcacafb5
wenlonglu <yummybroccoli@outlook.com>
2024-09-16 05:36:31 +0800

    博客增删改查及退出

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -17,14 +17,14 @@ const jwtSecret = process.env.JWT_SECRET
 const authMiddleware = (req, res, next) => {
     const token = req.cookies.token
     if (!token) {
-        return res.status(401).json({ message: '无权进入梦境'}) // 改成一个没有登录权限的页面 更好看
+        return res.status(401).json({ message: '无权进入梦境'}) // 改成一个没有登录权限的页面 更好看 添加按钮 去登录
     }
     try {
         const decoded = jwt.verify(token, jwtSecret)
         req.userId = decoded.userId
         next()
     } catch (error) {
-        res.status(401).json({ message: '无权进入梦境'}) // 改成一个没有登录权限的页面 更好看
+        res.status(401).json({ message: '无权进入梦境'}) // 改成一个没有登录权限的页面 更好看 添加按钮 去登录
     }
 }
 
@@ -73,7 +73,100 @@ router.post('/admin', async (req, res) => {
  * Admin - Dashboard
  */
 router.get('/dashboard', authMiddleware, async (req, res) => {
-    res.render('admin/dashboard')
+    try {
+        const locals = {
+            title: '梦旅人之界',
+            description: '在这里，我们用文字谱写心灵的乐章，探索思想的深度与广度。'
+        }
+        const data = await Post.find()
+        res.render('admin/dashboard', {
+            locals,
+            data,
+            layout: adminLayout
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+/**
+ * GET /
+ * Admin - Create New Post
+ */
+router.get('/add-post', authMiddleware, async (req, res) => {
+    try {
+        const locals = {
+            title: '记录梦境',
+            description: '在这里，我们用文字谱写心灵的乐章，探索思想的深度与广度。'
+        }
+        const data = await Post.find()
+        res.render('admin/add-post', {
+            locals,
+            layout: adminLayout
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+/**
+ * POST /
+ * Admin - Create New Post
+ */
+router.post('/add-post', authMiddleware, async (req, res) => {
+    try {
+        try {
+            const newPost = new Post({
+                title: req.body.title,
+                body: req.body.body
+            })
+            await Post.create(newPost)
+            res.redirect('/dashboard')
+
+        } catch (error) {
+            console.log(error)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+/**
+ * GET /
+ * Admin - Edit Post
+ */
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+        const locals = {
+            title: '梦语重塑',
+            description: '在这里，我们用文字谱写心灵的乐章，探索思想的深度与广度。'
+        }
+        const data = await Post.findOne({ _id: req.params.id })
+        res.render('admin/edit-post', {
+            locals,
+            data,
+            layout: adminLayout
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+/**
+ * PUT /
+ * Admin - Edit Post
+ */
+router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+        await Post.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            body: req.body.body,
+            updatedAt: Date.now()
+        })
+        res.redirect(`/edit-post/${req.params.id}`)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 /**
@@ -97,6 +190,28 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         console.log(error)
     }
+})
+
+/**
+ * DELETE /
+ * Admin - Delete Post
+ */
+router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
+    try {
+        await Post.deleteOne({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.error
+    }
+})
+
+/**
+ * GET /
+ * Admin - Logout
+ */
+router.get('/logout', async (req, res) => {
+    res.clearCookie('token')
+    res.redirect('/')
 })
 
 module.exports = router
